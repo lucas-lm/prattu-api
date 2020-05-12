e372f0262308c6b183c63f6a22de541088063446
Lucas Miranda <lucas-m@outlook.com>
2020-05-11 22:10:54 -0300

    chore: removed console.log

--- src/app/controllers/KitchenController.js
+++ src/app/controllers/KitchenController.js
@@ -13,7 +13,6 @@ module.exports = {
     try {
       const [role] = await Role.findOrCreate({ where: { name: 'kitchen' } })
       const user = await User.findByPk(id)
-      console.log(name, legal_id, location, avatar)
       const point = JSON.parse(location)
       await user.createKitchen({ name, legal_id, avatar, location: point })
       await user.addRole(role)
